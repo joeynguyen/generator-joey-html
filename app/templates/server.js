97a7356dd61ddb19971a2726b28a5593a224474a
Joey Nguyen <joey.nguyen@dealertrack.com>
2015-09-24 19:46:57 -0500

    Added route to static assets.

--- app/templates/server.js
+++ app/templates/server.js
@@ -12,6 +12,7 @@ var port = 3000;
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+app.use(express.static(path.join(__dirname, '/')));
 
 app.listen(port);
 console.log('Listening at http://localhost:' + port);
